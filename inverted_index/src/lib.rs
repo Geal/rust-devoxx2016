db2bda58fff1eed8a7a0c55b371b282636186840
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-07 15:25:05 +0100

    create the rust index struct

--- inverted_index/src/lib.rs
+++ inverted_index/src/lib.rs
@@ -1,6 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::collections::{HashMap,HashSet};
+
+pub struct Index {
+  pub index: HashMap<String, HashSet<i32>>,
+}
+
+impl Index {
+  pub fn new() -> Index {
+    Index {
+      index: HashMap::new(),
     }
+  }
+
+  pub fn insert(&mut self, id: i32, data: &str) {
+    
+  }
+
+  pub fn search_word(&self, word: &str) -> Option<&HashSet<i32>> {
+    self.index.get(word)
+  }
 }

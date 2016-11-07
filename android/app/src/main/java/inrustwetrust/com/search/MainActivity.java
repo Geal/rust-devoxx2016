c2babf03787f3d9ae59cc6f454040f84d557a084
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-07 14:34:42 +0100

    show the results

--- android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
+++ android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
@@ -3,6 +3,7 @@ package inrustwetrust.com.search;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     Button   searchButton;
     EditText searchBox;
     TextView text;
+    Index    index;
+    ArrayList<Talk> talks;
 
     String loadJSON(String filename) {
         String json = null;
@@ -50,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 String room_id = slot.getString("roomId");
                 Log.d("search", "room: "+room_id);
                 JSONObject talk = slot.optJSONObject("talk");
-                //slot.optJSONObject()
                 if(talk == null) {
                     Log.d("search", "no talk here");
                 } else {
@@ -64,16 +66,10 @@ public class MainActivity extends AppCompatActivity {
                         speaker_list += ", "+name;
                     }
 
-
                     Log.d("search", title+ " - "+speaker_list+": "+summary);
                     Talk t = new Talk(speaker_list, title, summary);
                     talks.add(t);
                 }
-
-                /*cat_name = jArray.getJSONObject(i).getString("cat_name");
-                Log.v("Cat ID", cat_Id);
-                Log.v("Cat Name", cat_name);
-                data.add(new String[] { cat_Id, cat_name });*/
             }
 
         } catch (JSONException e) {
@@ -81,6 +77,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return talks;
+    }
+
+    void displaySearch(String query) {
+        Log.d("search", "searching for \""+query+"\"");
+        Set<Integer> res = index.searchString(query);
+        Log.d("search", "results for \""+query+"\": " + res.toString());
+        String s = "results for \"java build\": " +Integer.toString(res.size())+" talks\n";
+        for(Integer i: res) {
+            Talk t = talks.get(i);
+            Log.d("search", t.speakerList+": "+ t.title + " - " + t.summary);
+            s += t.title + "\n";
+            s += t.summary + "\n\n";
+        }
+
+        text.setText(s);
+        Log.d("search", "found "+Integer.toString(res.size())+" talks");
     }
 
     @Override
@@ -92,34 +104,28 @@ public class MainActivity extends AppCompatActivity {
         searchBox    = (EditText) findViewById(R.id.search_box);
         searchButton = (Button) findViewById(R.id.search_button);
 
-        ArrayList<Talk> talks = new ArrayList<>();
+        talks = new ArrayList<>();
         talks.addAll(loadTalks(loadJSON("monday.json")));
         talks.addAll(loadTalks(loadJSON("tuesday.json")));
         talks.addAll(loadTalks(loadJSON("wednesday.json")));
         talks.addAll(loadTalks(loadJSON("thursday.json")));
         talks.addAll(loadTalks(loadJSON("friday.json")));
 
-        Index index = new Index();
+        index = new Index();
         for(int i = 0; i < talks.size(); i++) {
             index.insert(i, talks.get(i).summary);
         }
 
-        for (Map.Entry<String, HashSet<Integer>> entry : index.getIndex().entrySet()) {
-            Log.d("search", entry.getKey() + ": "+entry.getValue().toString());
-        }
-
-        Set<Integer> res = index.searchString("java build");
-        Log.d("search", "results for \"java build\": " + res.toString());
-        String s = "results for \"java build\": " +Integer.toString(res.size())+" talks\n";
-        for(Integer i: res) {
-            Talk t = talks.get(i);
-            Log.d("search", t.title + " - " + t.summary);
-            s += t.title + "\n";
-            s += t.summary + "\n\n";
-        }
-
-        text.setText(s);
+        displaySearch("java build");
 
         Log.d("search", Integer.toString(talks.size())+" talks stored");
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = searchBox.getText().toString();
+                displaySearch(query);
+            }
+        });
     }
 }

91ea2a3d45f303200e60e5ea5661150f0e91c6b9
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-07 11:57:21 +0100

    load json files

--- android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
+++ android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
@@ -2,12 +2,41 @@ package inrustwetrust.com.search;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    String loadJSON(String filename) {
+        String json = null;
+        try {
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String monday = loadJSON("monday.json");
+        Log.d("search", "json file: "+monday);
+        TextView txt = (TextView) findViewById(R.id.text);
+        txt.setText(monday);
+
     }
 }

82914b110ef6baa8c21761f916768224aedaa383
Geoffroy Couprie <geo.couprie@gmail.com>
2016-11-07 15:00:13 +0100

    display the results in a listview

--- android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
+++ android/app/src/main/java/inrustwetrust/com/search/MainActivity.java
@@ -1,11 +1,14 @@
 package inrustwetrust.com.search;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -15,16 +18,18 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.RunnableFuture;
 
 public class MainActivity extends AppCompatActivity {
     Button   searchButton;
     EditText searchBox;
     TextView text;
+    ListView result_list;
     Index    index;
     ArrayList<Talk> talks;
+    ArrayList<String> results;
+    ArrayAdapter<String> adapter;
 
     String loadJSON(String filename) {
         String json = null;
@@ -51,14 +56,14 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < slots.length(); i++) {
                 JSONObject slot = slots.getJSONObject(i);
                 String room_id = slot.getString("roomId");
-                Log.d("search", "room: "+room_id);
+                //Log.d("search", "room: "+room_id);
                 JSONObject talk = slot.optJSONObject("talk");
                 if(talk == null) {
-                    Log.d("search", "no talk here");
+                    //Log.d("search", "no talk here");
                 } else {
                     String title   = talk.getString("title");
                     String summary = talk.getString("summary");
-                    Log.d("search", "title: "+title);
+                    //Log.d("search", "title: "+title);
                     JSONArray speakers = talk.getJSONArray("speakers");
                     String speaker_list = speakers.getJSONObject(0).getString("name");
                     for (int j = 1; j < speakers.length(); j++) {
@@ -66,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                         speaker_list += ", "+name;
                     }
 
-                    Log.d("search", title+ " - "+speaker_list+": "+summary);
+                    //Log.d("search", title+ " - "+speaker_list+": "+summary);
                     Talk t = new Talk(speaker_list, title, summary);
                     talks.add(t);
                 }
@@ -81,18 +86,30 @@ public class MainActivity extends AppCompatActivity {
 
     void displaySearch(String query) {
         Log.d("search", "searching for \""+query+"\"");
-        Set<Integer> res = index.searchString(query);
+        final Set<Integer> res = index.searchString(query);
         Log.d("search", "results for \""+query+"\": " + res.toString());
-        String s = "results for \"java build\": " +Integer.toString(res.size())+" talks\n";
+        String s = "results for \"\"+query+\"\": " +Integer.toString(res.size())+" talks\n";
+        results.clear();
         for(Integer i: res) {
             Talk t = talks.get(i);
             Log.d("search", t.speakerList+": "+ t.title + " - " + t.summary);
             s += t.title + "\n";
             s += t.summary + "\n\n";
+
+            String s2 = t.title + "\n\n" + t.summary;
+            results.add(s2);
         }
 
-        text.setText(s);
-        Log.d("search", "found "+Integer.toString(res.size())+" talks");
+        final String txt = s;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(txt);
+                Log.d("search", "found "+Integer.toString(res.size())+" talks");
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
     @Override
@@ -102,23 +119,44 @@ public class MainActivity extends AppCompatActivity {
 
         text         = (TextView) findViewById(R.id.text);
         searchBox    = (EditText) findViewById(R.id.search_box);
-        searchButton = (Button) findViewById(R.id.search_button);
+        searchButton = (Button)   findViewById(R.id.search_button);
+        result_list  = (ListView) findViewById(R.id.search_results);
 
-        talks = new ArrayList<>();
-        talks.addAll(loadTalks(loadJSON("monday.json")));
-        talks.addAll(loadTalks(loadJSON("tuesday.json")));
-        talks.addAll(loadTalks(loadJSON("wednesday.json")));
-        talks.addAll(loadTalks(loadJSON("thursday.json")));
-        talks.addAll(loadTalks(loadJSON("friday.json")));
+        results = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                results);
 
-        index = new Index();
-        for(int i = 0; i < talks.size(); i++) {
-            index.insert(i, talks.get(i).summary);
-        }
+        result_list.setAdapter(adapter);
 
-        displaySearch("java build");
+        new AsyncTask<Object, Object, Void>(){
+            @Override
+            protected Void doInBackground(Object... params) {
+                talks = new ArrayList<>();
+                talks.addAll(loadTalks(loadJSON("monday.json")));
+                talks.addAll(loadTalks(loadJSON("tuesday.json")));
+                talks.addAll(loadTalks(loadJSON("wednesday.json")));
+                talks.addAll(loadTalks(loadJSON("thursday.json")));
+                talks.addAll(loadTalks(loadJSON("friday.json")));
 
-        Log.d("search", Integer.toString(talks.size())+" talks stored");
+                index = new Index();
+                for(int i = 0; i < talks.size(); i++) {
+                    index.insert(i, talks.get(i).summary);
+                }
+
+                displaySearch("java build");
+
+                Log.d("search", Integer.toString(talks.size())+" talks stored");
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void s) {
+
+            }
+        }.execute();
+
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
